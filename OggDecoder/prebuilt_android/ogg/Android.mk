b67d967957e01010c2d9c1e504efe343e16ad40b
Xrysnow <xrysnow@outlook.com>
2020-03-10 00:32:13 +0800

    remove unused files

--- OggDecoder/prebuilt_android/ogg/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := libogg-prebuilt
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../$(TARGET_ARCH_ABI)/libogg.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
-include $(PREBUILT_SHARED_LIBRARY)
--- OggDecoder/prebuilt_android/vorbis/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := libvorbis-prebuilt
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../$(TARGET_ARCH_ABI)/libvorbis.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
-include $(PREBUILT_SHARED_LIBRARY)
